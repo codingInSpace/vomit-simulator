3f1035f517aee229f005c80d951fec64d87c0bdf
codingInSpace <jonathan.grangien@gmail.com>
2016-06-07 22:07:51 +0200

    Implement annoying bg color hue variation script

---
+++ Assets/Scripts/bgColorVariator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bgColorVariator : MonoBehaviour {
+	
+	void Update () {
+		float H, S, V;
+		Color current = this.GetComponent<Camera>().backgroundColor;
+		Color.RGBToHSV(current, out H, out S, out V);
+		H += 0.01f;
+
+		this.GetComponent<Camera>().backgroundColor = Color.HSVToRGB(H, S, V);
+	}
+}
